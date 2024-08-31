850272ca6f9fc64c0b70fb94dbaa1be48a10f949
Nguyễn Tuấn <67007848+anhtuan14072002@users.noreply.github.com>
2024-08-31 18:59:11 +0700

    Add files via upload

---
+++ JP1/src/Jp1/HomoSapien.java
@@ -0,0 +1,19 @@
+package Jp1;
+
+public class HomoSapien {
+    public int weight;
+    public int height;
+    public int age;
+
+    public HomoSapien(int weight, int height, int age) {
+        this.weight = weight;
+        this.height = height;
+        this.age = age;
+    }
+    public void jumping(){
+        System.out.println("jumping");
+    }
+    public void higher(){
+        System.out.println("higher");
+    }
+}
